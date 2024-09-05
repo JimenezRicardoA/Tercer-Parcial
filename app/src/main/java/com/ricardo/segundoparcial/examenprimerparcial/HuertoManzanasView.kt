6dd5332a3926ee9e6b7e0a5cffcfb5b007a51917
Ricardo Jiménez Madero <r.jimenez0134@gmail.com>
2024-09-05 03:02:17 -0600

    Se agregan los cuadros de texto y la otra imagen al codigo de Examen Primer Parcial

--- app/src/main/java/com/ricardo/segundoparcial/examenprimerparcial/HuertoManzanasView.kt
+++ app/src/main/java/com/ricardo/segundoparcial/examenprimerparcial/HuertoManzanasView.kt
@@ -3,25 +3,38 @@ package com.ricardo.segundoparcial.examenprimerparcial
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.ricardo.segundoparcial.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HuertoManzanasView(viewModel: HuertoManzanasViewModel, navController: NavController){
+    val ProduccionTotal by viewModel.getTotal().observeAsState(0)
+    val ProduccionActual by viewModel.getActual().observeAsState(0)
+
     Box{
         TopAppBar(
             title = { Text(stringResource(id = R.string.tExamenPrimerParcial), color = Color.White) },
@@ -41,9 +54,65 @@ fun HuertoManzanasView(viewModel: HuertoManzanasViewModel, navController: NavCon
 
         Column(
             modifier = Modifier.align(Alignment.Center)
-                .offset(y = (-50).dp)
+                .offset(y = (-20).dp)
         ){
-            Text("Huerto de Manzanas", color = Color.Black)
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text ("Produccion Total",
+                    color = Color.Black,
+                    style = TextStyle(fontSize = 17.sp),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                        .offset(x = (30).dp)
+                )
+                Text ("${ProduccionTotal}",
+                    color = Color.Black,
+                    style = TextStyle(fontSize = 25.sp),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                        .offset(x = (103).dp)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.manzana),
+                    contentDescription = "Descripción de la imagen",
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .offset(x = (170).dp)
+                        .size(50.dp)
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text ("Produccion Actual",
+                    color = Color.Black,
+                    style = TextStyle(fontSize = 17.sp),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                        .offset(x = (30).dp)
+                )
+                Text ("${ProduccionActual}",
+                    color = Color.Black,
+                    style = TextStyle(fontSize = 25.sp),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                        .offset(x = (93).dp)
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.manzana),
+                    contentDescription = "Descripción de la imagen",
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .offset(x = (160).dp)
+                        .size(50.dp)
+                )
+
+            }
 
         }
     }
--- app/src/main/java/com/ricardo/segundoparcial/examenprimerparcial/HuertoManzanasViewModel.kt
+++ app/src/main/java/com/ricardo/segundoparcial/examenprimerparcial/HuertoManzanasViewModel.kt
@@ -1,7 +1,13 @@
 package com.ricardo.segundoparcial.examenprimerparcial
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HuertoManzanasViewModel : ViewModel(){
+    val actual = MutableLiveData (0)
+    val Total = MutableLiveData (0)
 
+    fun getActual() : LiveData<Int> = actual
+    fun getTotal() : LiveData<Int> = Total
 }
